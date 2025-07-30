545fe451a3dbf9bdf7ef1e3276b159fd8c261a81
cecixileichen <136110675+xileicecichen@users.noreply.github.com>
2025-07-30 17:23:33 +0200

    domain change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Artist-Website/', // This should match your GitHub repository name
+  base: '/', 
   build: {
     outDir: 'dist',
     assetsDir: 'assets'

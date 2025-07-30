35c7969c79caa9e88b67dc8e5056d20452f4c368
cecixileichen <136110675+xileicecichen@users.noreply.github.com>
2025-07-30 14:56:52 +0200

    Debugging fix to index.html and main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css' // (your global styles)
 
 ReactDOM.createRoot(document.getElementById('root')).render(

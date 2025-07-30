21c4f7a646077bdbceb37f97e518c4633f29e4cf
Xilei Ceci Chen <136110675+xileicecichen@users.noreply.github.com>
2025-07-30 14:45:24 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css' // (your global styles)
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { HashRouter } from 'react-router-dom';
+import './styles/index.css'; // Make sure your global CSS is imported here
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
-)
+);
